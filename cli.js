acd859c1c7dbb79ded96dd64657e74926553adf4
Mohamed Azzouni <mohamed.azzouni@gmail.com>
2021-06-03 12:00:37 +0200

    Use exclusively python3

--- cli.js
+++ cli.js
@@ -5,7 +5,7 @@ const path = require('path')
 const {spawn} = require('child_process')
 const [,, ... args] = process.argv
 
-const child = spawn('python  -u  '+path.join(__dirname, 'easinGenerator.py'), args, {
+const child = spawn('python3  -u  '+path.join(__dirname, 'easinGenerator.py'), args, {
   stdio: 'inherit',
   shell: true
 });
